6d89310c5e65aea560a5bab676f93ec246242ed1
VitaliiTymko <intehmet2015@gmail.com>
2022-12-26 22:35:14 +0200

    finish task2

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,10 +4,27 @@ import { FaRegThumbsUp } from 'react-icons/fa';
 import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
 import { GiTreeDoor } from 'react-icons/gi';
 
-export const Statistics = () => {
+const icons = [
+  <FaRegThumbsUp />,
+  <MdPeople />,
+  <MdOutlineProductionQuantityLimits />,
+  <GiTreeDoor />,
+];
+
+export const Statistics = ({ title, stats }) => {
   return (
     <>
-      <div>StatisticsList</div>
+      {title && <StatisticTitle>{title}</StatisticTitle>}
+      <StatisticsList>
+        {stats.map((item, index) => (
+          <StatisticItem
+            key={item.id}
+            title={item.title}
+            total={item.total}
+            icon={icons[index]}
+          />
+        ))}
+      </StatisticsList>
     </>
   );
 };
