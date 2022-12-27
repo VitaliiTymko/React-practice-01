74108300f639555a5fbab7e890bcccf0e08fff11
VitaliiTymko <intehmet2015@gmail.com>
2022-12-27 23:49:33 +0200

    finish task3

--- src/components/ForbesList/ForbesList.jsx
+++ src/components/ForbesList/ForbesList.jsx
@@ -8,6 +8,27 @@ import {
   LeaderBoardProfiles,
 } from './ForbesList.styled';
 
-export const ForbesList = () => {
-  return <div>ForbesList</div>;
+export const ForbesList = ({ forbes }) => {
+  return (
+    <LeaderBoard>
+      <BoardHeader>
+        <BoardTitle>
+          <TitleTop>Forbes</TitleTop>
+          <TitleBottom>Leader board</TitleBottom>
+        </BoardTitle>
+      </BoardHeader>
+
+      <LeaderBoardProfiles>
+        {forbes?.map(item => (
+          <ForbesListItem
+            key={item.id}
+            avatar={item.avatar}
+            name={item.name}
+            capital={item.capital}
+            isIncrease={item.isIncrease}
+          />
+        ))}
+      </LeaderBoardProfiles>
+    </LeaderBoard>
+  );
 };
